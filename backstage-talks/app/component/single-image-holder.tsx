5152e3a418a15fa1fb0d395c428daaa7080d99ba
nkp1111 <neerajp4321@gmail.com>
2024-02-04 17:09:44 +0530

    feat:💄✨ add scroll to different section

---
+++ backstage-talks/app/component/single-image-holder.tsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import BarcodeSvg from './barcode-svg'
+import Image from 'next/image'
+
+export default function SingleImageHolder({ index, imageColor }: { index: number, imageColor: string }) {
+  return (
+    <div className='h-[100vh] flex flex-col items-center p-5'>
+      <div className='w-[350] h-[450] relative mt-10 rounded-md rounded-e-none shadow-xl'>
+        <Image
+          src={`https://placehold.co/350x450/${imageColor}/${index === 1 ? "000" : "FFF"}/png?font=Roboto&text=Backstage Talks`}
+          alt={"book image"}
+          width={350}
+          height={450}
+          className='transition-all duration-200 ease-linear'
+        />
+        <div className={`absolute top-0 left-2 bottom-0 ${index === 1 ? "border-black" : "border-white"} border opacity-10 transition-all duration-200 ease-linear`}></div>
+        <small className={`absolute top-2 right-2 text-xs ${index === 1 ? "text-black" : "text-white"} transition-all duration-200 ease-linear`}>
+          Dialogues on Design and business
+        </small>
+
+        <div className={`w-20 h-20 rounded-full absolute right-0 top-10 translate-x-1/2 ${index === 1 ? "bg-orange-600" : "bg-black"} flex items-center justify-center text-white text-base`}>
+          Issue {index + 1}
+        </div>
+
+        <small className={`absolute bottom-2 left-0 px-5 text-[8px] ${index === 1 ? "text-black" : "text-white"}
+         transition-all duration-200 ease-linear flex justify-between w-full items-center overflow-hidden`}>
+          <span className='w-10 h-5 bg-white p-1 flex justify-center items-center'>
+            <BarcodeSvg />
+          </span>
+          <span className="flex gap-3">
+            <span>Backstage Talks/By Design Magazine</span>
+            <span className='font-bold'>Issue {index + 1}</span>
+          </span>
+        </small>
+      </div>
+
+      <div className='flex flex-col items-center'>
+        <h2 className='font-medium text-xl text-black my-5'>Issue #{index + 1}</h2>
+        <a href="#" className={`${index === 1 ? "text-orange-600" : "text-white"} font-bold text-lg uppercase hover:underline`}>Buy Here</a>
+        <p className='mt-3'>or in <a href="#" className={`${index === 1 ? "text-orange-600" : "text-white"} hover:underline`}>selected stores</a></p>
+      </div>
+    </div>
+  )
+}
--- backstage-talks/app/page.tsx
+++ backstage-talks/app/page.tsx
@@ -2,7 +2,7 @@
 
 import { Oswald } from "next/font/google";
 import "./globals.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import ScrollImages from "./component/scroll-images";
 
 const inter = Oswald({ weight: ["700"], subsets: ["latin"] });
@@ -10,12 +10,18 @@ const inter = Oswald({ weight: ["700"], subsets: ["latin"] });
 export default function Home() {
   const [currentView, setCurrentView] = useState(0);
   const issueColors = ["bg-pink-500", "bg-white", "bg-green-500", "bg-orange-500", "bg-sky-500", "bg-red-500", "bg-violet-500"];
+  useEffect(() => {
+    const scrollEl = document.querySelector(".scroll-el") as HTMLElement;
+    if (scrollEl) {
+      scrollEl.style.transform = `translateY(-${window.innerHeight * currentView}px)`
+    }
+  }, [currentView]);
 
   return (
-    <main className={`h-[100vh] p-5 flex flex-col bg- justify-between text-lg ${issueColors[currentView]} transition-all duration-300 ease-linear`}>
+    <main className={`h-[100vh] p-5 flex flex-col justify-between text-lg ${issueColors[currentView]} transition-all duration-300 ease-linear  relative`}>
       <div className="flex-1 flex justify-between">
         <div className="flex-1">
-          <h1 className={`uppercase font-boldtext-4xl ${inter.className}`}>backstage talks</h1>
+          <h1 className={`uppercase font-bold text-4xl ${inter.className}`}>backstage talks</h1>
         </div>
         <div className="flex-1 text-end">
           <a href="#" className="font-semibold hover:underline transition-all duration-200 ease-linear ">info@backstagetalks.com</a>
@@ -30,14 +36,14 @@ export default function Home() {
           <p className="max-w-[40%] font-semibold">
             Backstage Talks is a magazine of casual, but in depth dialogues on design and business. Our decisions shape and influence this complex world—to have a chance to make the right ones, we need to talk.
           </p>
-          <p className="text-sm mb-5 mt-1 text-gray-600">&copy; 2024
+          <p className={`text-sm mb-5 mt-1 ${currentView === 1 ? "text-gray-600" : "text-gray-200"}`}>&copy; 2024
             <a href="#" className="hover:underline"> Published by Büro Milk</a>
           </p>
           <p className="font-bold"> <a href="#" className="hover:no-underline underline">Privacy Policy</a></p>
         </div>
         <div className="flex-1 text-end flex flex-col-reverse">
           {Array(7).fill(0).map((_, index) => (
-            <a key={index} href="#" className={`hover:underline ${currentView === index ? "font-bold" : "font-normal"}`}
+            <a key={index} href={`#`} className={`hover:underline ${currentView === index ? "font-bold" : "font-normal"}`}
               onClick={() => setCurrentView(index)}
             >Issue # {index + 1}</a>
           ))}
