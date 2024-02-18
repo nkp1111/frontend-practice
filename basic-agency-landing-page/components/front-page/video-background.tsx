469e88375c3387672da54415edf803328601a836
nkp1111 <neerajp4321@gmail.com>
2024-02-18 16:12:12 +0530

    feat:💄✨ add custom cursor on home page

--- basic-agency-landing-page/components/front-page/video-background.tsx
+++ basic-agency-landing-page/components/front-page/video-background.tsx
@@ -1,9 +1,32 @@
 "use client";
 
-import React from 'react'
+import React, { useRef } from 'react'
 import ReactPlayer from 'react-player'
 
 export default function VideoBackground() {
+  const cursorRef = useRef<HTMLDivElement>(null);
+
+  const setCursorPos = (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    /**
+     * @desc set custom cursor position to cursor location on div overlay
+     */
+    const xPos = event.clientX;
+    const yPos = event.clientY;
+    const customCursorEl = cursorRef.current;
+    if (!customCursorEl) return;
+    customCursorEl.style.top = yPos + "px";
+    customCursorEl.style.left = xPos + "px";
+  }
+
+  const resetCursorPos = () => {
+    /**
+     * @desc resets custom cursor position to div overlay center
+     */
+    const customCursorEl = cursorRef.current;
+    if (!customCursorEl) return;
+    customCursorEl.style.top = "50%";
+    customCursorEl.style.left = "50%";
+  }
 
   return (
     <div className='absolute top-0 left-0 right-0 md:h-full h-3/4 bg-custom-dark flex justify-center items-start overflow-hidden -z-0'>
@@ -18,7 +41,25 @@ export default function VideoBackground() {
           height='120%'
         />
       </div>
-      <div className='absolute top-0 left-0 right-0 bottom-0 bg-transparent z-10'></div>
+
+      {/* cursor-[url("https://placehold.co/100x100/fff/000?text=Watch++Reel"),_pointer] */}
+      <div className='absolute top-0 left-0 right-0 bottom-0 bg-transparent z-10 hover:cursor-none'
+        onMouseOver={setCursorPos}
+        onMouseEnter={setCursorPos}
+        onMouseMove={setCursorPos}
+        onMouseLeave={resetCursorPos}
+      ></div>
+
+      {/* custom cursor */}
+      <div className={`flex flex-col absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2`} ref={cursorRef}>
+        <div className='w-28 h-28 rounded-full bg-custom-light text-custom-dark shadow-sm flex justify-center items-center font-semibold text-center uppercase'>
+          Watch<br />Reel
+        </div>
+        <div className='text-center font-bold text-custom-light mt-2'>
+          <span className='uppercase'>Basic/Dept</span> <br />
+          <span className='text-xs -mt-3'>2010-&#8734;</span>
+        </div>
+      </div>
     </div>
   )
 }
